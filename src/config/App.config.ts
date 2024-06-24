3002cbad3d9f1861feb59c4f50b5981a4d30e90b
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2024-06-24 16:00:37 +0530

    update

--- src/config/App.config.ts
+++ src/config/App.config.ts
@@ -1,3 +1,4 @@
+// export const REACT_APP_BASE_URL: string = "http://15.206.100.176:4005";
 export const REACT_APP_BASE_URL: string = "http://localhost:4005";
 export const REACT_APP_SECRET_KEY: string = "Eyul6X4uytwbDS&EA7Cv0Xj5C1*%j|BA5DwskcPnvex44q1WjsFr";
 export const REACT_APP_PRODUCT_PER_PAGE: number = 15;
