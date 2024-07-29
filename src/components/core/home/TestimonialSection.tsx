f4c94a8ce81698f4afe4f4a767458ed0794e2905
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2024-07-29 18:12:11 +0530

    Update

--- src/components/core/home/TestimonialSection.tsx
+++ src/components/core/home/TestimonialSection.tsx
@@ -20,67 +20,68 @@ const TestimonialSection = (): JSX.Element => {
 
     return (
         <>
-            <section className="testimonial style-2 pt-70">
-                <div className="container">
-                    <div className="section-header w-100 wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s">
-                        <h2>Testimonials</h2>
-                        <h2><span> Listen to what my clients say</span></h2>
-                    </div>
-                    <div className="section-wrapper">
-                        <div className="testimonial-slider">
-                            <Swiper
-                                modules={[Navigation, Autoplay]}
-                                slidesPerView={1}
-                                spaceBetween={20}
-                                loop={true}
-                                autoplay={{
-                                    delay: 3200,
-                                    disableOnInteraction: false,
-                                }}
-                                pagination={{
-                                    el: '.testimonial-pagination',
-                                    clickable: true,
-                                }}
-                                navigation={{
-                                    nextEl: '.testimonial-button-next',
-                                    prevEl: '.testimonial-button-prev',
-                                }}
-                                breakpoints={{
-                                    576: {
-                                        spaceBetween: 0,
-                                    },
-                                }}
-                            >
-                                {
-                                    testimonials_data?.length > 0 &&
-                                    testimonials_data?.map((item: UserMessage) =>
-                                        item?.is_highlighted && (
-                                            <SwiperSlide key={item?._id}>
-                                                <div className="swiper-slide">
-                                                    <div className="testi-item">
-                                                        <div className="testi-content">
-                                                            <img src="assets/images/testimonial/icon/03.png" alt="testimonial" />
-                                                            <p>{item?.message}</p>
-                                                            <div className="testimonial-author">
-                                                                <div className="author-name-des">
-                                                                    <h4>{item?.full_name}</h4>
-                                                                    <p>{item?.designation}</p>
+            {testimonials_data?.length > 0 &&
+                <section className="testimonial style-2 pt-70">
+                    <div className="container">
+                        <div className="section-header w-100 wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s">
+                            <h2>Testimonials</h2>
+                            <h2><span> Listen to what my clients say</span></h2>
+                        </div>
+                        <div className="section-wrapper">
+                            <div className="testimonial-slider">
+                                <Swiper
+                                    modules={[Navigation, Autoplay]}
+                                    slidesPerView={1}
+                                    spaceBetween={20}
+                                    loop={true}
+                                    autoplay={{
+                                        delay: 3200,
+                                        disableOnInteraction: false,
+                                    }}
+                                    pagination={{
+                                        el: '.testimonial-pagination',
+                                        clickable: true,
+                                    }}
+                                    navigation={{
+                                        nextEl: '.testimonial-button-next',
+                                        prevEl: '.testimonial-button-prev',
+                                    }}
+                                    breakpoints={{
+                                        576: {
+                                            spaceBetween: 0,
+                                        },
+                                    }}
+                                >
+                                    {
+                                        testimonials_data?.map((item: UserMessage) =>
+                                            item?.is_highlighted && (
+                                                <SwiperSlide key={item?._id}>
+                                                    <div className="swiper-slide">
+                                                        <div className="testi-item">
+                                                            <div className="testi-content">
+                                                                <img src="assets/images/testimonial/icon/03.png" alt="testimonial" />
+                                                                <p>{item?.message}</p>
+                                                                <div className="testimonial-author">
+                                                                    <div className="author-name-des">
+                                                                        <h4>{item?.full_name}</h4>
+                                                                        <p>{item?.designation}</p>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </SwiperSlide>
+                                                </SwiperSlide>
+                                            )
                                         )
-                                    )
-                                }
-                            </Swiper>
-                            <div className="testimonial-button-prev"><i className="fas fa-chevron-left"></i></div>
-                            <div className="testimonial-button-next"><i className="fas fa-chevron-right"></i></div>
+                                    }
+                                </Swiper>
+                                <div className="testimonial-button-prev"><i className="fas fa-chevron-left"></i></div>
+                                <div className="testimonial-button-next"><i className="fas fa-chevron-right"></i></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            }
         </>
     );
 };
