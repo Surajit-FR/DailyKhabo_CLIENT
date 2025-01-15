6ab99032f6454a56512d9fc20d974b3ded4ff580
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2025-01-15 20:08:37 +0530

    product drop

--- src/config/App.config.ts
+++ src/config/App.config.ts
@@ -1,5 +1,5 @@
-// export const REACT_APP_BASE_URL: string = "https://voguenvogue.org";
-export const REACT_APP_BASE_URL: string = "http://localhost:4005";
+export const REACT_APP_BASE_URL: string = "https://voguenvogue.org";
+// export const REACT_APP_BASE_URL: string = "http://localhost:4005";
 export const REACT_APP_SECRET_KEY: string = "Eyul6X4uytwbDS&EA7Cv0Xj5C1*%j|BA5DwskcPnvex44q1WjsFr";
 export const REACT_APP_PRODUCT_PER_PAGE: number = 15;
 export const REACT_APP_RECAPTCHA_SITE_KEY: string = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
